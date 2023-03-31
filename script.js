1a0948c5f8642f23df9e01b2c02af26f68987892
Avinash-sahu404 <115649844+Avinash-sahu404@users.noreply.github.com>
2023-03-31 15:08:06 +0530

    Nee

--- script.js
+++ script.js
@@ -79,3 +79,7 @@ $(function() {
         
     });
  });
+     setInterval(function(){ $(".custom-social-proof").stop().slideToggle('slow'); }, 8000);
+      $(".custom-close").click(function() {
+        $(".custom-social-proof").stop().slideToggle('slow');
+      });
