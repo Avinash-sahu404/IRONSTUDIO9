0281847afd37cd10bba1addf65c07789a04d34a2
Avinash-sahu404 <115649844+Avinash-sahu404@users.noreply.github.com>
2023-03-31 15:48:35 +0530

    Done

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ $(function() {
         
     });
  });
-     setInterval(function(){ $(".custom-social-proof").stop().slideToggle('slow'); }, 8000);
+     setInterval(function(){ $("custom-social-proof").stop().slideToggle('slow'); }, 8000);
       $(".custom-close").click(function() {
-        $(".custom-social-proof").stop().slideToggle('slow');
+        $("custom-social-proof").stop().slideToggle('slow');
       });
